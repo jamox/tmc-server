e4f8fd1df48e910aa7de89ad7f878d6e661f24b0
Jarmo Isotalo <jamo@isotalo.fi>
2013-11-10 00:58:30 +0200

    more logic, less tests

--- lib/student_events_recorder_app.rb
+++ lib/student_events_recorder_app.rb
@@ -1,9 +1,15 @@
 class StudentEventsRecorderApp
 
+
   def call(env)
     raw_response = nil
     @req = Rack::Request.new(env)
     @resp = Rack::Response.new
+    puts "\n\nstarting"
+
+    @http_basic = @req.env["HTTP_AUTHORIZATION"]
+   # puts @req.params.inspect
+    puts "done \n\n"
     if @req.path.end_with?('.html')
       @resp['Content-Type'] = 'text/html; charset=utf-8'
       @respdata = ''
@@ -78,5 +84,70 @@ class StudentEventsRecorderApp
       @resp.status = 500
       @respdata[:status] = 'busy'
     end
+  end
+
+  def handle_events
+    user_id = get_user_id(@req.env["HTTP_AUTHORIZATION"])
+
+    params = @req.params
+    event_records = params['events'].values
+
+    File.open(params['data'].tempfile.path, 'rb') do |data_file|
+      ex_map = {}
+      Exercise.includes(:course).each do |ex|
+        ex_map["#{ex.course.name} #{ex.name}"] = ex
+      end
+
+      event_records.each do |record|
+        course_id = get_course_id(record['course_name'])
+        exercise_name = get_exercise_name_within_course(course_id, record['exercise_name'])
+
+        event_type = record['event_type']
+        metadata = record['metadata']
+        happened_at = record['happened_at']
+        system_nano_time = record['system_nano_time']
+
+        data_file.pos = record['data_offset'].to_i
+        data = data_file.read(record['data_length'].to_i)
+
+        #unless StudentEvent.supported_event_types.include?(event_type)
+        #  raise "Invalid event type: '#{event_type}'"
+        #end
+
+        #check_json_syntax(metadata) if metadata
+
+        event = {
+          :user_id => user_id,
+          :course_id => course_id,
+          :exercise_name => exercise_name,
+          :event_type => event_type,
+          :metadata_json => metadata,
+          :data => data,
+          :happened_at => happened_at,
+          :system_nano_time => system_nano_time
+        }
+        save_event(event)
+      end
+    end
+  end
+
+  # TODO: implement
+  def get_exercise_name_within_course(course, ex_name)
+    # SELECT "exercises".* FROM "exercises" WHERE "exercises"."course_id" = 1 AND "exercises"."name" = 'viikko3-3.1.Polynomi' LIMIT 1
+  end
+
+  # TODO: implement
+  def get_course_id(course_name)
+    # SELECT "courses".* FROM "courses" WHERE "courses"."name" = 'tira-k2012-paja' LIMIT 1
+  end
+
+  # TODO: implement real functionality!!!
+  def get_user_id(http_basic_auth)
+    return 1
+  end
+  # TODO: implement
+  def save_event(event)
+
+
   end
 end
