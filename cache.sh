5dc2397f0922801c2cedf1d9d6462b3aa55e3a5f
Jarmo Isotalo <jamo@isotalo.fi>
2015-10-26 00:31:27 +0200

    moar logging

--- cache.sh
+++ cache.sh
@@ -6,5 +6,7 @@ then
 else
   echo $(git submodule status ext/tmc-sandbox | grep -E -o  "[0-9a-f]{40}")
   wget  http://testmycode.net/travis/sandbox-$(git submodule status ext/tmc-sandbox | grep -E -o  "[0-9a-f]{40}").tar.gz -O sandbox.tar.gz
+  file sandbox.tar.gz
+  stat sandbox.tar.gz
   tar xvz sandbox.tar.gz -C ext/
 fi
