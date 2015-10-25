1d382233c9cdc263a31336bcd14770ecdc71d4a5
Jarmo Isotalo <jamo@isotalo.fi>
2015-10-26 00:13:17 +0200

    cache2

--- cache.sh
+++ cache.sh
@@ -6,5 +6,5 @@ then
 else
   echo $(git submodule status ext/tmc-sandbox | grep -E -o  "[0-9a-f]{40}")
   wget  http://testmycode.net/travis/sandbox-$(git submodule status ext/tmc-sandbox | grep -E -o  "[0-9a-f]{40}").tar.gz -O sandbox.tar.gz
-  tar xz sandbox.tar.gz -C ext/
+  tar xvz sandbox.tar.gz -C ext/
 fi
