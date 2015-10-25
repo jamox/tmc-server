fc6bbcf0d265bbdc96e29dc1695ae2af91830faf
Jarmo Isotalo <jamo@isotalo.fi>
2015-10-25 23:57:15 +0200

    cache

---
+++ cache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+SANDBOX=$(git submodule status ext/tmc-sandbox | grep -E -o  "[0-9a-f]{40}")
+if [ -d sandbox-cache/$SANDBOX ]
+then
+  cp sandbox-cache/$SANDBOX/{linux.uml,initrd.img,rootfs.squashfs} ext/tmc-sandbox/uml
+else
+  echo $(git submodule status ext/tmc-sandbox | grep -E -o  "[0-9a-f]{40}")
+  wget  http://testmycode.net/travis/sandbox-$(git submodule status ext/tmc-sandbox | grep -E -o  "[0-9a-f]{40}").tar.gz -O sandbox.tar.gz
+  tar xz sandbox.tar.gz -C ext/
+fi
